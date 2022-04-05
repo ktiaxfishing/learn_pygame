dfb2e579a266db06addaeaed4e0d2ad2b1b879a5
ktiaxfishing@gmail.com <kitaxfishing@gmail.com>
2022-04-05 14:13:16 +0700

    game

--- game_8.py
+++ game_8.py
@@ -5,6 +5,10 @@ import random
 import pygame
 from glom import Glom
 pygame.init()
+font = pygame.font.SysFont('Commic SansMS',30)
+texttriender = font.render("hello",False,(255,0,0))
+x = 0
+
 
 # # Set up the drawing window
 screen = pygame.display.set_mode([500, 500])
@@ -12,6 +16,9 @@ screen = pygame.display.set_mode([500, 500])
 print(Glom, type(Glom))
 c = Glom()
 print('success')
+score = 0
+hold = False
+fire = False
 
 # Run until the user asks to quit
 running = True
@@ -27,9 +34,35 @@ while running:
 
 #     # Fill the background with white
     screen.fill((255, 255, 255))
+    x += 1
+    xx = str(x)
+    textrender = font.render(xx,False,(255,0,0))
+    screen.blit(textrender,(100,100))
 
 #     # draw
-    c.draw(screen)
+    left = pygame.mouse.get_pressed()[0]
+    fire = False
+    if left == True and hold == False:
+        fire = True
+        hold = True
+    if left == False and hold == True:
+        hold = False
+
+    # if hold == True and left == False:
+    #     hold = False
+    mouse_pos = pygame.mouse.get_pos()
+    point = c.draw(screen,fire,mouse_pos)
+    if point == "+1":
+        score +=1
+    elif point == "-1":
+        score -=1
+    elif point == "-0.5":
+        score -= 0.5
+    print(score,hold,fire)
+    score_ = c.draw(screen,mouse_pos,left)
+    if score_ =="+1":
+        score+=1
+    pygame.draw.circle(screen, (255,0,0), (mouse_pos[0], mouse_pos[1]),5)
 #     # Flip the display
     pygame.display.flip()
 
--- glom.py
+++ glom.py
@@ -7,15 +7,38 @@ class Glom:
     #     print('hello from glom')
 
     def __init__(self):
-        self.x = random.randint(1,500) 
-        self.y = random.randint(1,500)
+        self.cen_x = random.randint(1,500) 
+        self.cen_y = random.randint(1,500)
         self.size =20
 
         
-    def draw(self,screen):
-        self.size += 0.1
+    def draw(self,screen,left,mouse_pos):
+        self.size += 0.01
         if self.size >=50:
             self.__init__()
-        pygame.draw.circle(screen, (0,0,255), (self.x, self.y), self.size)
+            return "-0.5"
+        elif left == True:
+            if self.is_clicked(mouse_pos) == True:
+                self.__init__()
+                return "+1"
+            else:
+                return "-1"
+        pygame.draw.circle(screen, (0,0,255), (self.cen_x, self.cen_y), self.size)
+        
         
       
+    def is_clicked(self,mouse_pos):
+        # radius, center, mouse
+        # check for clicked
+            # find x, y 
+            # find diagonal
+            # if diagonal less than radius mean hit
+        # return true or false
+
+        x = self.cen_x - mouse_pos[0]
+        y = self.cen_y - mouse_pos[1]
+        diagonal = (x**2 + y**2)**0.5
+        if diagonal <= self.size:
+            return True
+        else:
+            return False
